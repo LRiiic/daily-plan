6cbdd9854e1e0facffd990df30da23fee8b9abb0
Luiz Ricardo <ricardo_santos-2010@hotmail.com>
2024-03-08 17:53:19 -0300

    Adding PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Daily Plan',
+        short_name: 'Daily Plan',
+        description: "Personal daily app",
+        background_color: '#ffe6bc',
+        theme_color: '#ffe6bc',
+        orientation: "portrait-primary",
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
